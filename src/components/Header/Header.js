acb020da1e1d87cff8813ced6683a9af2709c470
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2023-10-04 10:42:59 -0500

    create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './Header.css';
 
 export default function Header(){
-
+  let currentTime = new Date().toLocaleString('default', {month: 'long', day: 'numeric'});
+  let location = "Bedlam";
   return (
     <div className='header'>
-      <img className='header__logo' src='/images/logo.svg' alt='logo'/>
-      <p className='header__date'>3023.13.35</p>
-      <button className='header__button add-clothes-button' type='button'>+Add clothes.</button>
-      <p className='header__username'>Meduka the Guca</p>
-      <div className='header__avatar'></div>
-    
-    
+      <div className='header__container'>
+        <img className='header__logo' src='/images/logo.svg' alt='logo'/>
+        <p className='header__date'>{currentTime}, {location}</p>
+      </div>
+      <div className='header__container'>
+        <button className='header__button add-clothes-button' type='text'>+Add clothes</button>
+        <p className='header__username'>Meduka the Guca</p>
+        <div className='header__avatar'></div>
+      </div>
     </div>
-  )
-  
+  ) 
 }
