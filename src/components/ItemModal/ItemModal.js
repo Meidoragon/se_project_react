cea83885b6ca8bea52f98432121035900b8ab2ee
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2023-11-08 15:08:33 -0600

    add horizontal layout for item modal

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -2,7 +2,7 @@ import './ItemModal.css';
 
 export default function ItemModal({item, onClose, isHorizontal = false}) {
   const verticalLayout = (
-    <div className={`image-modal`}>
+    <div className='image-modal'>
       <div className='image-modal__content'>
       <button className='image-modal__close-button' type="button" onClick={onClose} />
         <img className='image-modal__image' src={item.link} alt={item.name}></img>
@@ -12,9 +12,14 @@ export default function ItemModal({item, onClose, isHorizontal = false}) {
     </div>
   )
   const horizontalLayout = (
-    <div className={`image-modal`}>
-      <div className='image-modal__content'>
-        <p>hi.</p>
+    <div className='image-modal'>
+      <div className='image-modal__content content_is-horizontal'>
+        <button className='image-modal__close-button close-button_is-horizontal' type='button' onClick={onClose} />
+        <div className='image-modal__horizontal-image-container'>
+          <img className='image-modal__image image_is-horizontal' src={item.link} alt={item.name} />
+          <p className='image-modal__text text_image-overlay'>{item.name}</p>
+        </div> 
+        <p className='image-modal__text'>Weather: {item.weather}</p>
       </div>
     </div>
   )
