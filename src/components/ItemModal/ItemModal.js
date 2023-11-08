43777d98dc4f42b98b2986bf7e4672e1a1ff4903
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2023-11-08 12:56:27 -0600

    apply styling for vertical item popup

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,13 +1,23 @@
-export default function ItemModal({item, onClose}) {
-  console.log(item);
-  return (
-    <div className={`modal modal_opened`}>
-      <div className='modal__content'>
-      <button className='modal__close-button' type="button" onClick={onClose} />
-        <img src={item.link} alt={item.name}></img>
-        <div>{item.name}</div>
-        <div>Weather type: {item.weather}</div>
+import './ItemModal.css';
+
+export default function ItemModal({item, onClose, isHorizontal = false}) {
+  const verticalLayout = (
+    <div className={`image-modal`}>
+      <div className='image-modal__content'>
+      <button className='image-modal__close-button' type="button" onClick={onClose} />
+        <img className='image-modal__image' src={item.link} alt={item.name}></img>
+        <p className='image-modal__text'>{item.name}</p>
+        <p className='image-modal__text'>Weather: {item.weather}</p>
       </div>
     </div>
   )
+  const horizontalLayout = (
+    <div className={`image-modal`}>
+      <div className='image-modal__content'>
+        <p>hi.</p>
+      </div>
+    </div>
+  )
+  return isHorizontal ? horizontalLayout : verticalLayout;
+  
 }
