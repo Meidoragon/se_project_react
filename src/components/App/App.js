2be509ea7c44b48f4bf64ec93ba6dba9fd6b20e5
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2023-11-30 21:38:17 -0600

    react routing init

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import '../../vendor/normalize.css';
 import Header from '../Header/Header.js';
 import Main from '../Main/Main.js';
 import Footer from '../Footer/Footer.js';
+import Profile from '../Profile/Profile.js';
 import { defaultClothingItems, defaultAPIInfo, radioOptions } from '../../utils/constants';
 import ModalWithForm from '../ModalWithForm/ModalWithForm';
 import ItemModal from '../ItemModal/ItemModal.js';
@@ -77,47 +79,54 @@ export default function App() {
   }, [activeModal])
   
   return (
-    <div className='page'>
+    <BrowserRouter>
       <CurrentTempUnitContext.Provider value ={{isTempUnitC: isTempUnitC, handleTempUnitSwitch}}>
+        <div className='page'>
         <Header locationName={location} userName={userName} openGarmentForm={openGarmentForm}/>
-        <Main clothingItems={defaultClothingItems}
-          temperature={temperature}
-          time={isDay ? 'day' : 'night'}
-          weather={weather}
-          onCardSelection={openCardPopup}/>
-        <Footer />
-        {activeModal === 'create' && (
-        <ModalWithForm title='New garment' name="new-garment" buttonText='Add garment' onClose={closePopup} onOverlayClick={handleOverlay} onSubmit={submitGarmentForm}>
-          <label className='form-modal__input-label'>
-            Name
-            <input className='form-modal__input' type='text' name='name' minLength='1' maxLength='30' placeholder='Name'/>
-          </label>
-          <label className='form-modal__input-label'>
-            Image
-            <input className="form-modal__input" type='url' id='formInputLink' name='link' minLength='1' placeholder='Image URL'/>
-          </label>
-          <fieldset className="form-modal__radio-buttons">
-            <legend className='form-modal__radio-title'>Select the weather type:</legend>
-            {radioOptions.map((choice, index) => {
-            return (
-              <div key={index}>
-                <input 
-                  className='form-modal__radio-button' 
-                  type='radio' 
-                  id={choice.value} 
-                  value={choice.value} 
-                  name='weather'
-                />
-                <label  className='form-modal__radio-button-label' htmlFor={choice.value}>{choice.text}</label>
-              </div>
-            )
-            })}
-          </fieldset>
-        </ModalWithForm>)}
-        {activeModal === 'preview' && 
-          <ItemModal item={selectedCard} onClose={closePopup} onOverlayClick={handleOverlay} />
-        }
+        <Route exact path='/profile'>
+          <Profile />
+        </Route>
+        <Route path='/'>
+          <Main clothingItems={defaultClothingItems}
+            temperature={temperature}
+            time={isDay ? 'day' : 'night'}
+            weather={weather}
+            onCardSelection={openCardPopup}/>
+          <Footer />
+          {activeModal === 'create' && (
+          <ModalWithForm title='New garment' name="new-garment" buttonText='Add garment' onClose={closePopup} onOverlayClick={handleOverlay} onSubmit={submitGarmentForm}>
+            <label className='form-modal__input-label'>
+              Name
+              <input className='form-modal__input' type='text' name='name' minLength='1' maxLength='30' placeholder='Name'/>
+            </label>
+            <label className='form-modal__input-label'>
+              Image
+              <input className="form-modal__input" type='url' id='formInputLink' name='link' minLength='1' placeholder='Image URL'/>
+            </label>
+            <fieldset className="form-modal__radio-buttons">
+              <legend className='form-modal__radio-title'>Select the weather type:</legend>
+              {radioOptions.map((choice, index) => {
+              return (
+                <div key={index}>
+                  <input 
+                    className='form-modal__radio-button' 
+                    type='radio' 
+                    id={choice.value} 
+                    value={choice.value} 
+                    name='weather'
+                  />
+                  <label  className='form-modal__radio-button-label' htmlFor={choice.value}>{choice.text}</label>
+                </div>
+              )
+              })}
+            </fieldset>
+          </ModalWithForm>)}
+          {activeModal === 'preview' && 
+            <ItemModal item={selectedCard} onClose={closePopup} onOverlayClick={handleOverlay} />
+          }
+        </Route>
+        </div>
       </CurrentTempUnitContext.Provider>
-    </div>
+    </BrowserRouter>
   );
 }
