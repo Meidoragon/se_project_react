e8f00a7396f28393ef9b03fbdf1c6b458e8a11fb
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-02-08 14:54:13 -0600

    UPDATE: update profile to use avatar component and current user context

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Avatar.css';
 
-export default function Avatar({ user, additionalClasses }) {
+export default function Avatar({ user, additionalClasses = '' }) {
   return (
     <>
       {
-        false /*user.avatar*/ ?
+        user.avatar ?
           <img className={`avatar avatar__image ${additionalClasses}`} src={user.avatar} alt='user avatar' /> :
           <div className={`avatar avatar__default ${additionalClasses}`}>{user.name[0].toUpperCase()}</div>
       }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,17 @@
 import "./Profile.css";
 import React from 'react';
+import { useContext } from "react";
 import Sidebar from '../Sidebar/Sidebar.js';
 import ClothesSection from '../ClothesSection/ClothesSection.js';
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 export default function Profile({ clothingItems, userName, avatar, openNewGarmentForm, createClothingCards }) {
+  const { user } = useContext(CurrentUserContext);
   return (
     <div className='profile'>
-      <Sidebar userName={userName} avatar={avatar} />
+      <Sidebar user={user} userName={userName} avatar={avatar} />
       <ClothesSection clothingItems={clothingItems} openNewGarmentForm={openNewGarmentForm} createClothingCards={createClothingCards} />
     </div>
   )
 }
+
