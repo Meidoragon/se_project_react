e5a412c545c31b4edde7ba235342818bed0190d2
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-02-08 14:44:01 -0600

    ADD: Avatar component for sensible default avatar

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Avatar.css';
+
+export default function Avatar({ user, additionalClasses }) {
+  return (
+    <>
+      {
+        false /*user.avatar*/ ?
+          <img className={`avatar avatar__image ${additionalClasses}`} src={user.avatar} alt='user avatar' /> :
+          <div className={`avatar avatar__default ${additionalClasses}`}>{user.name[0].toUpperCase()}</div>
+      }
+    </>
+  )
+}
