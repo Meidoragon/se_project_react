9c6bca8f1b97f4c652f4a11dafedcc64eb6a200b
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-02-08 14:54:13 -0600

    UPDATE: update profile to use avatar component and current user context

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,12 @@
 import './Sidebar.css';
+import Avatar from '../Avatar/Avatar';
 
-export default function Sidebar({ userName, avatar }) {
+export default function Sidebar({ user, userName, avatar }) {
   return (
     <div className='sidebar'>
       <div className='sidebar__user-info'>
-        <img className='sidebar__user-avatar' alt='user avatar' src={avatar} />
-        <div className='sidebar__user-name'>{userName}</div>
+        <Avatar user={user} />
+        <div className='sidebar__user-name'>{user.name}</div>
       </div>
     </div>
   )
