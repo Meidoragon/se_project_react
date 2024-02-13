c374f03017e0b602c94920eacd0bf6dca324d3e3
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-02-13 11:42:46 -0600

    FIX: fix exports from variable name change

--- src/contexts/CurrentTemperatureUnitContext.js
+++ src/contexts/CurrentTemperatureUnitContext.js
@@ -1,5 +1,7 @@
 import React from "react";
-export const CurrentTempUnitContext = React.createContext({
-  isTempUnitC: false,
-  handleTempUnitSwitch: () => { },
+export const CurrentTemperatureUnitContext = React.createContext({
+  currentTemperatureUnit: 'celsius',
+  handleToggleSwitchChange: () => { },
+  // isTempUnitC: false,
+  // handleTempUnitSwitch: () => { },
 });
